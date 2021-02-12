6d70dbfa1f38df59572bee72a9cec486ec966ad7
NadyaSwan <lazorkonadya@gmail.com>
2021-02-12 22:36:19 +0200

    test: add cases for input forms

--- cypress/integration/input-form.spec.ts
+++ cypress/integration/input-form.spec.ts
@@ -1,33 +1,54 @@
 import pages from '../pages';
 
+const message = 'Test message to type!';
+
 describe('Input form checks', () => {
-  context('general', () => {
-    it('should see input form fields', () => {
-      cy.visit('/');
-      pages.basePage
-        .banner()
-        .should('be.visible')
-        .within(() => {
-          cy.contains('No, thanks!').should('be.visible').click();
-        });
-      pages.basePage.treeMenu().should('be.visible');
-      pages.basePage
-        .treeBranches()
-        .should('be.visible')
-        .and('have.length', 7)
-        .eq(0)
-        .click();
-      pages.basePage
-        .inputFormExamples()
-        .should('be.visible')
-        .and('have.length', 7)
-        .eq(0)
-        .click();
-      pages.inputFormsPage.singleInputField().should('be.visible');
-      pages.inputFormsPage.twoInputFields().should('be.visible');
+  beforeEach(() => {
+    cy.visit('/');
+    pages.basePage
+      .banner()
+      .should('be.visible')
+      .within(() => {
+        cy.contains('No, thanks!').should('be.visible').click();
+      });
+    pages.basePage.treeMenu().should('be.visible');
+    pages.basePage
+      .treeBranches()
+      .should('be.visible')
+      .and('have.length', 7)
+      .eq(0)
+      .click();
+    pages.basePage
+      .inputFormExamples()
+      .should('be.visible')
+      .and('have.length', 7)
+      .eq(0)
+      .click();
+    pages.inputFormsPage.singleInputField().should('be.visible');
+    pages.inputFormsPage.twoInputFields().should('be.visible');
+  });
+
+  context('Single input field', () => {
+    it('should fill input with words and show them in message on submit', () => {
+      pages.inputFormsPage.userMessageInput().fillWith(message);
+      cy.contains('button', 'Show Message').click();
+      pages.inputFormsPage.userMessage().should('have.text', message);
     });
   });
-  context('valid', () => {
-    it('should fill input with not allowed chars', () => {});
+
+  context('Two input fields', () => {
+    it('should type integer numbers into two fields and see their SUM on submit', () => {
+      cy.log('Check SUM of two strings');
+      pages.inputFormsPage.firstValueInput().fillWith('string 1');
+      pages.inputFormsPage.secondValueInput().fillWith('string 2');
+      cy.contains('button', 'Get Total').click();
+      pages.inputFormsPage.valueMessage().should('have.text', 'NaN');
+
+      cy.log('Check SUM of two numbers');
+      pages.inputFormsPage.firstValueInput().fillWith('1');
+      pages.inputFormsPage.secondValueInput().fillWith('2');
+      cy.contains('button', 'Get Total').click();
+      pages.inputFormsPage.valueMessage().should('have.text', '3');
+    });
   });
 });
--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -1,3 +1,8 @@
 declare namespace Cypress {
-  interface Chainable {}
+  interface Chainable {
+    /** Command to clear input and type into the given value
+     * @text value you want to type
+     */
+    fillWith(text: string): Cypress.Chainable<JQuery>;
+  }
 }
